92ac13331a1489979ad7872d50315f4505da65d9
Eric Delmonico <ead1758@rit.edu>
2019-11-25 18:16:58 -0500

    did some error checking and improved search functionality by allowing a genre search

--- js/main.js
+++ js/main.js
@@ -104,13 +104,9 @@ function changeSort(e){
         sortString = "";
     }
     else{
-        // give the capability to sort using the url,
-        // but for now we're sorting the old fashioned way.
-        // When using the in-url method, all results (including
-        // the least relevant results) get sorted, and thus
-        // you get totally irrelevant anime at the top,
-        // simply because it has a higher score than 
-        // all the more relevant results
+        // this is used in the url during a genre search,
+        // which means the user types nothing in, instead
+        // just choosing a genre.
         let params = sortString.split("_");
         sortString = `&order_by=${params[0]}&sort=${params[1]}`;
     }
@@ -159,7 +155,6 @@ function search(){
     searchTerm = encodeURIComponent(searchTerm.trim());
     if (searchTerm.length <= 0) {
         queryTerm = "";
-        return;
     }
     else{
         queryTerm += searchTerm;
@@ -171,6 +166,9 @@ function search(){
     // if we got this far, add the term to the url
     let url = `https://api.jikan.moe/v3/search/anime?${queryTerm}${genreString}${pageString}`;
 
+    if (searchTerm.length == 0){
+        url += sortString;
+    }
     console.log(url);
 
     // show the loading cursor
@@ -195,6 +193,10 @@ function querySuccess(obj){
         // if something went wrong, save 
         // reason and abandon ship, yeet
         let msg = obj.error;
+        let error = document.createElement("p");
+        error.innerHTML = msg;
+        resultsContainer.appendChild(error);
+        searchOngoing = false;
         return;
     }
 
@@ -202,15 +204,27 @@ function querySuccess(obj){
     let resultsContainer = document.querySelector("#results");
     let results = obj.results;
 
-    if (results.length == 0){
+    // if there's a too short search term and no results, show error
+    if (searchTerm.length < 3 && results.length == 0){
         let error = document.createElement("p");
-        error.innerHTML = "No results found."
+        error.innerHTML = "Search term was less than three characters.";
         resultsContainer.appendChild(error);
         searchOngoing = false;
         return;
     }
 
-    if (sortString.length > 0){
+    // if there are no results, tell the user
+    if (results.length == 0){
+        let error = document.createElement("p");
+        error.innerHTML = "No results found.";
+        resultsContainer.appendChild(error);
+        searchOngoing = false;
+        return;
+    }
+
+    // if the search term has a length of zero,
+    // that means the user is just genre searching
+    if (sortString.length > 0 && searchTerm.length != 0){
         results.sort((a, b) => b.score - a.score);    
     }
 
